a7a29b8c8582199d95d2de9cf1aa91daa306b605
Anthony <anthonybilodeau214@gmail.com>
2021-07-07 11:12:01 -0400

    modified default values of debug env;

--- debug.py
+++ debug.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy
+import functools
 
 import gym
 import gym.spaces
@@ -188,8 +189,136 @@ def main():
         )
 
 
+def main_batch():
+    import logging
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", type=str, default="gym_sted:STEDdebugBleach-v0")
+    parser.add_argument("--seed", type=int, default=0, help="Random seed [0, 2 ** 32)")
+    parser.add_argument("--gpu", type=int, default=None)
+    parser.add_argument("--exp_id", type=str, default="debug")
+    parser.add_argument("--dry-run", action="store_true", default=False)
+    parser.add_argument(
+        "--outdir",
+        type=str,
+        default="results",
+        help=(
+            "Directory path to save output files."
+            " If it does not exist, it will be created."
+        ),
+    )
+    parser.add_argument("--batchsize", type=int, default=16)
+    parser.add_argument("--steps", type=int, default=10 ** 5)
+    parser.add_argument("--eval-interval", type=int, default=100)
+    parser.add_argument("--eval-n-runs", type=int, default=5)
+    parser.add_argument("--reward-scale-factor", type=float, default=1.)
+    parser.add_argument("--render", action="store_true", default=False)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--demo", action="store_true", default=False)
+    parser.add_argument("--load", type=str, default="")
+    parser.add_argument("--log-level", type=int, default=logging.INFO)
+    parser.add_argument("--monitor", action="store_true")
+    parser.add_argument("--num_envs", type=int, default=1)
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level)
+
+    # Set a random seed used in PFRL.
+    utils.set_random_seed(args.seed)
+
+    # Set different random seeds for different subprocesses.
+    # If seed=0 and processes=4, subprocess seeds are [0, 1, 2, 3].
+    # If seed=1 and processes=4, subprocess seeds are [4, 5, 6, 7].
+    process_seeds = numpy.arange(args.num_envs) + args.seed * args.num_envs
+    assert process_seeds.max() < 2 ** 32
+
+    args.outdir = experiments.prepare_output_dir(args, args.outdir, exp_id=args.exp_id, make_backup=not args.dry_run)
+
+    def make_env(idx, test):
+        # Use different random seeds for train and test envs
+        process_seed = int(process_seeds[idx])
+        env_seed = 2 ** 32 - 1 - process_seed if test else process_seed
+
+        env = gym.make(args.env)
+        # Use different random seeds for train and test envs
+        env.seed(env_seed)
+        # Cast observations to float32 because our model uses float32
+        env = pfrl.wrappers.CastObservationToFloat32(env)
+        if args.monitor:
+            env = pfrl.wrappers.Monitor(env, args.outdir)
+        if not test:
+            # Scale rewards (and thus returns) to a reasonable range so that
+            # training is easier
+            env = pfrl.wrappers.ScaleReward(env, args.reward_scale_factor)
+        if args.render and not test:
+            env = pfrl.wrappers.Render(env)
+        return env
+
+    def make_batch_env(test):
+        vec_env = pfrl.envs.MultiprocessVectorEnv(
+            [
+                functools.partial(make_env, idx, test)
+                for idx, env in enumerate(range(args.num_envs))
+            ]
+        )
+        # vec_env = pfrl.wrappers.VectorFrameStack(vec_env, 4)
+        return vec_env
+
+    sample_env = make_env(0, test=False)
+    timestep_limit = sample_env.spec.max_episode_steps
+    obs_space = sample_env.observation_space
+    action_space = sample_env.action_space
+
+    obs_size = obs_space.shape
+    policy = Policy(obs_size=obs_size)
+    vf = ValueFunction(obs_size=obs_size)
+    model = pfrl.nn.Branched(policy, vf)
+
+    opt = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+    agent = pfrl.agents.PPO(
+        model,
+        opt,
+        gpu=args.gpu,
+        minibatch_size=args.batchsize,
+        max_grad_norm=1.0,
+        update_interval=100
+    )
+    if args.load:
+        agent.load(args.load)
+
+    if args.demo:
+        eval_stats = experiments.eval_performance(
+            env=eval_env,
+            agent=agent,
+            n_steps=None,
+            n_episodes=args.eval_n_runs,
+            max_episode_len=timestep_limit,
+        )
+        print(
+            "n_runs: {} mean: {} median: {} stdev {}".format(
+                args.eval_n_runs,
+                eval_stats["mean"],
+                eval_stats["median"],
+                eval_stats["stdev"],
+            )
+        )
+    else:
+        experiments.train_agent_batch_with_evaluation(
+            agent=agent,
+            env=make_batch_env(test=False),
+            eval_env=make_batch_env(test=True),
+            outdir=args.outdir,
+            steps=args.steps,
+            eval_n_steps=None,
+            eval_n_episodes=args.eval_n_runs,
+            eval_interval=args.eval_interval
+        )
+
+
 if __name__ == "__main__":
 
     # Run the following line of code
     # python main.py --env gym_sted:STEDdebug-v0 --batchsize=16 --gpu=None --reward-scale-factor=1.0 --eval-interval=100 --eval-n-runs=5
-    main()
+    # main()
+    main_batch()
