6e25efba1eb873d6515b43941137f3f562f4a27a
Benoit Turcotte <benoiturcotte@gmail.com>
2021-08-17 12:03:24 -0400

    modified the models to use the LargeAtari NN instead of smallAtari, modified a couple of things for the action selection differences between gym1 and gym2

--- src/models.py
+++ src/models.py
@@ -16,7 +16,8 @@ def calc_shape(shape, layers):
 
     :returns : A `tuple` of the output shape
     """
-    _shape = numpy.array(shape[1:])
+    # _shape = numpy.array(shape[1:])
+    _shape = numpy.array(shape[-2:])
     for layer in layers:
         _shape = (_shape + 2 * numpy.array(layer.padding) - numpy.array(layer.dilation) * (numpy.array(layer.kernel_size) - 1) - 1) / numpy.array(layer.stride) + 1
         _shape = _shape.astype(int)
@@ -78,21 +79,20 @@ class Policy2(nn.Module):
     if Policy was so good, why isn't there a Policy 2?
     """
     def __init__(
-        self, in_channels=1, action_size=1, obs_space=None,
+        self, in_channels=1, action_size=1, obs_space=None, encoded_signal_shape=4,
         activation=nn.functional.leaky_relu
     ):
         self.in_channels = in_channels
-        # self.in_channels = in_channels
         self.action_size = action_size
         self.obs_space = obs_space
         self.activation = activation
         super(Policy2, self).__init__()
 
-        self.encoded_signal_shape = 4
+        self.encoded_signal_shape = encoded_signal_shape   # param d'entrée ?
         self.img_shape = (1, 64, 64)
 
         # mon obs_space est un tuple, maintenant il faut déterminer la shape du premier elt du tuple:
-        # c'est soit (64, 64, 1) (gym1) ou (64, 64, 4)
+        # c'est soit (1, 64, 64) (gym1) ou (4, 64, 64)
         # il faut stoquer cette info qqpart pour pouvoir faire le forward comme il faut
         if self.obs_space[0].shape[0] == 1:
             self.is_timed_env = False
@@ -104,10 +104,11 @@ class Policy2(nn.Module):
             nn.Conv2d(self.obs_space[0].shape[0], 1, 3, stride=1, padding=1)
         ])
 
-        # Image encoder (1 image to a vector)
+        # Image encoder (1 image to a vector) (This is the LargeAtari architecture)
         self.image_encoder_layers = nn.ModuleList([
-            nn.Conv2d(self.in_channels, 16, 8, stride=4),
-            nn.Conv2d(16, 32, 4, stride=2),
+            nn.Conv2d(self.in_channels, 32, 8, stride=4),
+            nn.Conv2d(32, 64, 4, stride=2),
+            nn.Conv2d(64, 64, 3, stride=1),
         ])
 
         # Signal encoder ([SNR, Resolution, Bleach] to a vector of length 4 (?)
@@ -116,31 +117,36 @@ class Policy2(nn.Module):
         ])
 
         out_shape = calc_shape(self.img_shape, self.image_encoder_layers)
-        in_features = 32 * numpy.prod(out_shape) + self.encoded_signal_shape
+        in_features = 64 * numpy.prod(out_shape) + self.encoded_signal_shape
+
         # Action selection
-        self.policy = nn.Sequential(
-            nn.Linear(in_features, action_size),
-            pfrl.policies.GaussianHeadWithStateIndependentCovariance(
+        self.policy_to_ranking_layer = nn.Linear(in_features, 1)
+        if self.is_timed_env:
+            self.policy_to_actions_layer = nn.Linear(in_features, action_size)
+            self.pfrl_head = pfrl.policies.GaussianHeadWithStateIndependentCovariance(
                 action_size=action_size,
                 var_type="diagonal",
                 var_func=lambda x: torch.exp(2 * x),  # Parameterize log std
                 var_param_init=0,  # log std = 0 => std = 1
             )
-        )
+        else:
+            self.policy_to_actions_layer = nn.Linear(in_features, action_size - 1)
+            self.pfrl_head = pfrl.policies.GaussianHeadWithStateIndependentCovariance(
+                action_size=action_size,   # + 1,
+                var_type="diagonal",
+                var_func=lambda x: torch.exp(2 * x),  # Parameterize log std
+                var_param_init=0,  # log std = 0 => std = 1
+            )
 
     def forward(self, x):
         # je crois que oui peu importe que ce soit gym 1 ou gym 2
         # si c'est gym 1, x = (img, [SNR, Resolution, Bleach, 1hot])
         # si c'est gym 2, x = ([4 imgs], [SNR, Resolution, Bleach, 1hot])
-        # print(self.obs_space)
-        # print(self.obs_space[0])
-        # print(self.obs_space[0].shape)
-        # exit()
         if isinstance(self.obs_space, gym.spaces.Tuple):
             x, articulation = x
 
         # passer x dans une couche qui send de 4 imgs vers 1
-        if x.shape[1] == 4:
+        if self.is_timed_env:
             for layer in self.recording_queue_encoder_layers:
                 x = self.activation(layer(x))
 
@@ -155,8 +161,15 @@ class Policy2(nn.Module):
 
         if isinstance(self.obs_space, gym.spaces.Tuple):
             x = torch.cat([x, articulation], dim=1)
-        x = self.policy(x)
-        # Anthony veut pouvoir retourner 2 types d'actions, est-ce que ça va marcher pour ça?
+
+        if self.is_timed_env:
+            x = self.policy_to_actions_layer(x)
+        else:
+            x_1 = self.policy_to_actions_layer(x)
+            x_2 = self.policy_to_ranking_layer(x)
+            x = torch.cat([x_1, x_2], dim=1)
+
+        x = self.pfrl_head(x)
         return x
 
 class ValueFunction(nn.Module):
@@ -211,21 +224,20 @@ class ValueFunction2(nn.Module):
     if ValueFunction was so good, why isn't there a ValueFunction 2?
     """
     def __init__(
-        self, in_channels=1, action_size=1, obs_space=None,
+        self, in_channels=1, action_size=1, obs_space=None, encoded_signal_shape=4,
         activation=torch.tanh
     ):
         self.in_channels = in_channels
-        # self.in_channels = in_channels
         self.action_size = action_size
         self.obs_space = obs_space
         self.activation = activation
         super(ValueFunction2, self).__init__()
 
-        self.encoded_signal_shape = 4
+        self.encoded_signal_shape = encoded_signal_shape
         self.img_shape = (1, 64, 64)
 
         # mon obs_space est un tuple, maintenant il faut déterminer la shape du premier elt du tuple:
-        # c'est soit (64, 64, 1) (gym1) ou (64, 64, 4)
+        # c'est soit (1, 64, 64) (gym1) ou (4, 64, 64)
         # il faut stoquer cette info qqpart pour pouvoir faire le forward comme il faut
         if self.obs_space[0].shape[0] == 1:
             self.is_timed_env = False
@@ -237,10 +249,11 @@ class ValueFunction2(nn.Module):
             nn.Conv2d(self.obs_space[0].shape[0], 1, 3, stride=1, padding=1)
         ])
 
-        # Image encoder (1 image to a vector)
+        # Image encoder (1 image to a vector) (This is the LargeAtari architecture)
         self.image_encoder_layers = nn.ModuleList([
-            nn.Conv2d(self.in_channels, 16, 8, stride=4),
-            nn.Conv2d(16, 32, 4, stride=2),
+            nn.Conv2d(self.in_channels, 32, 8, stride=4),
+            nn.Conv2d(32, 64, 4, stride=2),
+            nn.Conv2d(64, 64, 3, stride=1),
         ])
 
         # Signal encoder ([SNR, Resolution, Bleach] to a vector of length 4 (?)
@@ -249,11 +262,10 @@ class ValueFunction2(nn.Module):
         ])
 
         out_shape = calc_shape(self.img_shape, self.image_encoder_layers)
-        in_features = 32 * numpy.prod(out_shape) + self.encoded_signal_shape
-        self.linears = nn.ModuleList([
-            nn.Linear(in_features, 64),
-            nn.Linear(64, action_size)
-        ])
+        in_features = 64 * numpy.prod(out_shape) + self.encoded_signal_shape
+
+        self.linear1 = nn.Linear(in_features, 64)
+        self.linear2 = nn.Linear(64, action_size)
 
     def forward(self, x):
         # je crois que oui peu importe que ce soit gym 1 ou gym 2
@@ -263,7 +275,7 @@ class ValueFunction2(nn.Module):
             x, articulation = x
 
         # passer x dans une couche qui send de 4 imgs vers 1
-        if x.shape[1] == 4:
+        if self.is_timed_env:
             for layer in self.recording_queue_encoder_layers:
                 x = self.activation(layer(x))
 
@@ -279,6 +291,7 @@ class ValueFunction2(nn.Module):
         if isinstance(self.obs_space, gym.spaces.Tuple):
             x = torch.cat([x, articulation], dim=1)
 
-        for layer in self.linears:
-            x = self.activation(layer(x))
+        x = self.activation(self.linear1(x))
+        x = self.linear2(x)
+
         return x
