67cf643fb3c65a2b061fbf0e0056ffd65f5f416a
Anthony <anthonybilodeau214@gmail.com>
2023-03-23 07:44:47 -0400

    updated models;

--- src/models.py
+++ src/models.py
@@ -150,6 +150,81 @@ class Policy2(nn.Module):
         x = self.policy_to_actions_layer(x)
         x = self.pfrl_head(x)
         return x
+    
+class PooledPolicy(nn.Module):
+    """
+    if Policy was so good, why isn't there a Policy 2?
+    """
+    def __init__(
+        self, in_channels=1, action_size=1, obs_space=None, encoded_signal_shape=16,
+        activation=nn.functional.leaky_relu
+    ):
+        self.in_channels = in_channels
+        self.action_size = action_size
+        self.obs_space = obs_space
+        self.activation = activation
+        super(Policy2, self).__init__()
+
+        self.encoded_signal_shape = encoded_signal_shape   # param d'entrée ?
+        self.img_shape = (self.in_channels, 64, 64)
+
+        # RecordingQueue encoder (4 images to 1)
+        self.recording_queue_encoder_layers = nn.ModuleList([
+            nn.Conv2d(self.obs_space[0].shape[0], self.in_channels, 3, stride=1, padding=1)
+        ])
+
+        # Image encoder (1 image to a vector) (This is the LargeAtari architecture)
+        self.image_encoder_layers = nn.ModuleList([
+            nn.Conv2d(self.in_channels, 32, 8, stride=4),
+            nn.Conv2d(32, 64, 4, stride=2),
+            nn.Conv2d(64, 64, 3, stride=1),
+        ])
+        self.image_pool_layer = nn.AdaptiveMaxPool2d(1)
+
+        # Signal encoder ([SNR, Resolution, Bleach] to a vector of length 4 (?)
+        self.signal_encoder_layers = nn.ModuleList([
+            nn.Linear(self.obs_space[1].shape[0], 16),
+            nn.Linear(16, self.encoded_signal_shape)
+        ])
+
+        in_features = 64 + self.encoded_signal_shape
+
+        # Action selection
+        self.policy_to_actions_layer = nn.Linear(in_features, action_size)
+        self.pfrl_head = pfrl.policies.GaussianHeadWithStateIndependentCovariance(
+            action_size=action_size,
+            var_type="diagonal",
+            var_func=lambda x: torch.exp(2 * x),  # Parameterize log std
+            var_param_init=0,  # log std = 0 => std = 1
+        )
+
+    def forward(self, x):
+        # Split image and articulation
+        if isinstance(self.obs_space, gym.spaces.Tuple):
+            x, articulation = x
+
+        # Encode to single channel image
+        for layer in self.recording_queue_encoder_layers:
+            x = self.activation(layer(x))
+
+        # Encode image in AtariNetwork
+        for layer in self.image_encoder_layers:
+            x = self.activation(layer(x))
+        x = self.image_pool_layer(x)
+        x = x.view(x.size(0), -1)
+
+        # Encode articulation
+        for layer in self.signal_encoder_layers:
+            articulation = self.activation(layer(articulation))
+
+        # Combines encoded image and articulation
+        if isinstance(self.obs_space, gym.spaces.Tuple):
+            x = torch.cat([x, articulation], dim=1)
+
+        # Runs the encoded into the policy
+        x = self.policy_to_actions_layer(x)
+        x = self.pfrl_head(x)
+        return x    
 
 class PolicyWithSideAction(nn.Module):
     """
@@ -625,6 +700,75 @@ class ValueFunction2(nn.Module):
         x = self.linear2(x)
 
         return x
+    
+class PooledValueFunction(nn.Module):
+    """
+    if ValueFunction was so good, why isn't there a ValueFunction 2?
+    """
+    def __init__(
+        self, in_channels=1, action_size=1, obs_space=None, encoded_signal_shape=16,
+        activation=torch.tanh
+    ):
+        self.in_channels = in_channels
+        self.action_size = action_size
+        self.obs_space = obs_space
+        self.activation = activation
+        super(ValueFunction2, self).__init__()
+
+        self.encoded_signal_shape = encoded_signal_shape
+        self.img_shape = (self.in_channels, 64, 64)
+
+        # RecordingQueue encoder (4 images to 1)
+        self.recording_queue_encoder_layers = nn.ModuleList([
+            nn.Conv2d(self.obs_space[0].shape[0], self.in_channels, 3, stride=1, padding=1)
+        ])
+
+        # Image encoder (1 image to a vector) (This is the LargeAtari architecture)
+        self.image_encoder_layers = nn.ModuleList([
+            nn.Conv2d(self.in_channels, 32, 8, stride=4),
+            nn.Conv2d(32, 64, 4, stride=2),
+            nn.Conv2d(64, 64, 3, stride=1),
+        ])
+        self.image_pool_layer = nn.AdaptiveMaxPool2d(1)
+
+        # Signal encoder ([SNR, Resolution, Bleach] to a vector of length 4 (?)
+        self.signal_encoder_layers = nn.ModuleList([
+            nn.Linear(self.obs_space[1].shape[0], 16),
+            nn.Linear(16, self.encoded_signal_shape)
+        ])
+
+        in_features = 64 + self.encoded_signal_shape
+
+        self.linear1 = nn.Linear(in_features, 64)
+        self.linear2 = nn.Linear(64, action_size)
+
+    def forward(self, x):
+        # je crois que oui peu importe que ce soit gym 1 ou gym 2
+        # si c'est gym 1, x = (img, [SNR, Resolution, Bleach, 1hot])
+        # si c'est gym 2, x = ([4 imgs], [SNR, Resolution, Bleach, 1hot])
+        if isinstance(self.obs_space, gym.spaces.Tuple):
+            x, articulation = x
+
+        for layer in self.recording_queue_encoder_layers:
+            x = self.activation(layer(x))
+
+        # passer l'image dans le nn standard
+        for layer in self.image_encoder_layers:
+            x = self.activation(layer(x))
+        x = self.image_pool_layer(x)
+        x = x.view(x.size(0), -1)
+
+        # passer le signal dans une couche linéaire X --> 4
+        for layer in self.signal_encoder_layers:
+            articulation = self.activation(layer(articulation))
+
+        if isinstance(self.obs_space, gym.spaces.Tuple):
+            x = torch.cat([x, articulation], dim=1)
+
+        x = self.activation(self.linear1(x))
+        x = self.linear2(x)
+
+        return x    
 
 class RecurrentValueFunction(nn.Module, pfrl.nn.Recurrent):
     """
